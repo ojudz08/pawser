4c09684edb912e1ba3a7109562fc55339978f8b9
Ojelle Rogero <ojelle.rogero@gmail.com>
2024-07-03 07:44:22 +0800

    update utils.py io.py

--- pawser/io.py
+++ pawser/io.py
@@ -15,26 +15,22 @@ import os, sys
 init_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 sys.path.insert(1, os.path.join(init_dir, "pawser"))
 
-from utils import debug, timer, init_dir
+from utils import init_dir
 
 #class read_pdf:
 #    """file_path: Union[str, os.PathLike],
 #       page_num: int, output_format: str,  output_path: str"""
 
-class TimeWaster:
-    @debug
-    def __init__(self, max_num):
-        self.max_num = max_num
+@init_dir
+class read_pdf:
+    def __init__(self, name):
+        self.name = name
 
-    @timer
-    def waste_time(self, num_times):
-        for _ in range(num_times):
-            sum([number**2 for number in range(self.max_num)])
-    
-    @init_dir
-    def test(self):
-        
+    def print_info(self):
+        print(f"Name: {self.name}")
+        print(f"Initial Directory: {self.initial_directory}")
 
-
-tw = TimeWaster(1000)
-tw.waste_time(999)
+# Example usage
+if __name__ == "__main__":
+    obj = read_pdf("Example")
+    obj.print_info()
--- pawser/utils.py
+++ pawser/utils.py
@@ -1,44 +1,18 @@
-import functools
-import time
-
 import os, sys
+from functools import wraps
+
+def get_initial_directory():
+    """ Returns the initial directory of the current working directory"""
+    return os.path.abspath(os.path.dirname(sys.argv[0]))
 
 
 def init_dir(func):
-    """Get the initial project directory"""
-    @functools.wraps(func)
-    def wrapper_initdir(*args, **kwargs):
-        init_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
-        value = func(*args, **kwargs)
-        return value
-    return wrapper_initdir
+    original_init = func.__init__
 
+    @wraps(original_init)
+    def wrapper_initdir(self, *args, **kwargs):
+        self.initial_directory = get_initial_directory()
+        original_init(self, *args, **kwargs)
 
-
-
-
-def timer(func):
-    """Print the runtime of the decorated function"""
-    @functools.wraps(func)
-    def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter()
-        value = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        run_time = end_time - start_time
-        print(f"Finished {func.__name__}() in {run_time:.4f} secs")
-        return value
-    return wrapper_timer
-
-
-def debug(func):
-    """Print the function signature and return value"""
-    @functools.wraps(func)
-    def wrapper_debug(*args, **kwargs):
-        args_repr = [repr(a) for a in args]
-        kwargs_repr = [f"{k}={repr(v)}" for k, v in kwargs.items()]
-        signature = ", ".join(args_repr + kwargs_repr)
-        print(f"Calling {func.__name__}({signature})")
-        value = func(*args, **kwargs)
-        print(f"{func.__name__}() returned {repr(value)}")
-        return value
-    return wrapper_debug
+    func.__init__ = wrapper_initdir
+    return func
